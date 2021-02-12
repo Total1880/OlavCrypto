dd954f44acb67e69d1a2369c79e4fccbda594c06
Olav Hendrickx <olav.hendrickx@capgemini.com>
2021-02-12 16:26:59 +0100

    add popup to add/edit wallet

--- OlavCrypto/Pages/OverviewWalletsPage.xaml.cs
+++ OlavCrypto/Pages/OverviewWalletsPage.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace OlavCrypto.Pages
 {
@@ -20,9 +7,17 @@ namespace OlavCrypto.Pages
     /// </summary>
     public partial class OverviewWalletsPage : Page
     {
+        private PopupWindow _popupWindow;
+
+        public PopupWindow PopupWindow => _popupWindow ??= new PopupWindow();
         public OverviewWalletsPage()
         {
             InitializeComponent();
+        }
+
+        private void ButtonAddWalletClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+            PopupWindow.Show();
         }
     }
 }
---
+++ OlavCrypto/PopupWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using OlavCrypto.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace OlavCrypto
+{
+    /// <summary>
+    /// Interaction logic for PopupWindow.xaml
+    /// </summary>
+    public partial class PopupWindow : Window
+    {
+        private DetailsWalletPage _detailsWalletPage;
+
+        public DetailsWalletPage DetailsWalletPage => _detailsWalletPage ??= new DetailsWalletPage();
+        public PopupWindow()
+        {
+            InitializeComponent();
+            MainFrame.NavigationService.Navigate(DetailsWalletPage);
+        }
+    }
+}
