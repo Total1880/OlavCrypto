c079bf81e6f3de857e580243f2518f7c195990b7
Olav Hendrickx <olav.hendrickx@capgemini.com>
2021-02-12 15:27:43 +0100

    add overviewscreen

--- OlavCrypto/MainWindow.xaml.cs
+++ OlavCrypto/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OlavCrypto.Pages;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OlavCrypto
 {
@@ -20,9 +8,14 @@ namespace OlavCrypto
     /// </summary>
     public partial class MainWindow : Window
     {
+        private OverviewWalletsPage _overviewWalletsPage;
+
+        public OverviewWalletsPage OverviewWalletsPage => _overviewWalletsPage ??= new OverviewWalletsPage();
+
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.NavigationService.Navigate(OverviewWalletsPage);
         }
     }
 }
